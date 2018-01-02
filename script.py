df24ae64e91826126d616aa7eb212a96dc147d7d
dima <skaspi33@gmail.com>
2018-01-02 15:16:39 +0200

    main-version-sub

--- script.py
+++ script.py
@@ -8,14 +8,14 @@ Ransomware Detection Project
 Technion, Haifa, Israel
 
 Auxiliary script for counting the number of times a files with given extensions were modified/deleted.
-
-The threshold for Ransomware Attack is assigned to be 40 file modifications/deletions.
+Ransomware Attack will be detected if an arbitrary number of honey-pots files are changed.
+The number of modifications is stored in dedicated .bak file.
 """
 
 import os
+import subprocess
 import sys
 import time
-import subprocess
 
 
 def panic():
