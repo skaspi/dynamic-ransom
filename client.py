3fbb4469e4c53fc611db90f8d65c474053ae5818
dima <skaspi33@gmail.com>
2018-01-13 19:41:40 +0200

    timer addition

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+HOST = '13.65.99.48'    # The remote host
+PORT = 8080             # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
---
+++ server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 21:12:16 2017
+
+@authors Rafael,Dmitriy
+Ransomware Detection Project
+Technion, Haifa, Israel
+
+
+"""
+import socket
+
+
+def main():
+
+    HOST = 'localhost'  # Symbolic name meaning all available interfaces
+    PORT = 8080  # Arbitrary non-privileged port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                conn.sendall(data)
+
+
+if __name__ == '__main__':
+    main()
