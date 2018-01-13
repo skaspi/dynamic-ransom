7c2254d33fa5d0d421a43f75c96ebfefce6998ea
dima <skaspi33@gmail.com>
2018-01-13 20:11:05 +0200

    timer addition

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '13.65.99.48'    # The remote host
+HOST = 'localhost'    # The remote host
 PORT = 8080             # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
