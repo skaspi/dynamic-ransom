9b02583c5845eee7759e34bf2024a7c143a80a52
dima <skaspi33@gmail.com>
2018-01-15 16:59:05 +0200

    server paralleled

--- server.py
+++ server.py
@@ -12,11 +12,88 @@ Each request contains a list of suspicious links, which will be analyzed
 by our Sand-Box mechanism.
 A huge credit goes to Silver Moon and his post from the following link:
 http://www.binarytides.com/python-socket-server-code-example/
-We really appreciate it :))
 """
+import json
+import os
+import re
+import shutil
 import socket
 import sys
 import threading
+from urllib.request import urlopen
+
+import requests
+
+links = []
+connection = 0
+
+
+class thread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        print("Started: " + self.name)
+        url_download(self.threadID)
+
+
+def url_download(i):
+    """
+     Download single file from given url
+    """
+    global links
+    url = links[i]
+
+    if not os.path.exists(str(connection)):
+        try:
+            os.makedirs(str(connection))
+        except FileExistsError:
+            pass
+
+    folder = str(connection)
+    request = requests.get(url)
+    cd = request.headers.get('content-disposition')
+    if cd:
+        filename = get_filename_from_cd(cd)
+    else:
+        filename = url.rsplit('/', 1)[1]
+
+    with urlopen(url) as response, open(filename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+        out_file.close()
+        file_name = out_file.__str__()
+        shutil.move(os.getcwd() + "\\" + filename, os.getcwd() + "\\" + str(connection) + "\\" + filename)
+
+
+def get_filename_from_cd(cd):
+    """
+     Auxiliary function for extracting filename from content-disposition
+    """
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+
+
+def handle_links(raw_data):
+    """
+     Routine for handling the received downloadable links
+    """
+    global links
+    raw_data = raw_data.decode('ASCII')
+    raw_data = json.loads(raw_data)
+    threads = []
+    for i in range(len(raw_data)):
+        thread_ = thread(i, "worker #" + str(i))
+        threads.append(thread_)
+        links.append(raw_data[i])
+
+    for worker in threads:
+        worker.start()
 
 
 def client_thread(conn):
@@ -25,17 +102,21 @@ def client_thread(conn):
     """
     while True:
         data = conn.recv(1024)
-        reply = b'OK...' + data
+
         if not data:
             break
+
+        handle_links(data)
+        reply = b'OK...'
         conn.sendall(reply)
+
     conn.close()
 
 
 def main():
     HOST = 'localhost'
     PORT = 8080
-
+    global connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Socket created')
 
@@ -55,6 +136,7 @@ def main():
         conn, addr = s.accept()
         print('Connected with ' + addr[0] + ':' + str(addr[1]))
         t = threading.Thread(target=client_thread, args=(conn,))
+        connection += 1
         t.start()
     s.close()
 
